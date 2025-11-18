8f5018191e2f92451405ded099b966a699e54111
DanielFeng <danielf6635@nycstudents.net>
2025-11-18 12:50:08 -0500

    final

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,30 +1,58 @@
 public class APCalendar
 {
-    /** Returns true if year is a leap year and false otherwise. */
     private static boolean isLeapYear(int year)
-    { /* implementation not shown */ }
-    /** Returns the number of leap years between year1 and year2, inclusive.
-     * Precondition: 0 <= year1 <= year2
-     */
+    {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
     public static int numberOfLeapYears(int year1, int year2)
-    { /* to be implemented in part (a) */ }
-    /** Returns the value representing the day of the week for the first day of year,
-     * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
-     */
-    private static int firstDayOfYear(int year)
-    { /* implementation not shown */ }
-    /** Returns n, where month, day, and year specify the nth day of the year.
-     * Returns 1 for January 1 (month = 1, day = 1) of any year.
-     * Precondition: The date represented by month, day, year is a valid date.
-     */
-    private static int dayOfYear(int month, int day, int year)
-    { /* implementation not shown */ }
-    /** Returns the value representing the day of the week for the given date
-     * (month, day, year), where 0 denotes Sunday, 1 denotes Monday, ...,
-     * and 6 denotes Saturday.
-     * Precondition: The date represented by month, day, year is a valid date.
-     */
+    {
+        int totalLeapYears = 0;
+        for (int i = year1; i <= year2; i++) {
+            if (isLeapYear(i)) {
+                totalLeapYears++;
+            }
+        }
+        return  totalLeapYears;
+    }
+    public static int firstDayOfYear(int year)
+    {
+        int day = 6; //2000 Jan 1st Saturday
+        if (year > 2000) {
+            for (int i = 2000; i < year; i++) {
+                int days = isLeapYear(i) ? 366 : 365; //if isleapyear then 366 else 365
+                day = (day + days) % 7; //mod to get the actual week
+            }
+        } else {
+            for (int i = 1999; i >= year; i--) {
+                int days = isLeapYear(i) ? 366 : 365; //if isleapyear then 366 else 365
+                day = (day - days) % 7;
+                if (day < 0) day += 7 ;
+            }
+        }
+
+        return day;
+    }
+    public static int dayOfYear(int month, int day, int year)
+    {
+        int n = 0;
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (isLeapYear(year)) {
+            daysInMonth[1] = 29; //leap year so feb becomes 29
+        }
+
+        for (int i = 0; i < month - 1; i++) {
+            n += daysInMonth[i];
+        }
+        n += day;
+        return n;
+    }
     public static int dayOfWeek(int month, int day, int year)
-    { /* to be implemented in part (b) */ }
-// There may be instance variables, constructors, and other methods not shown.
+    {
+        int firstDay = firstDayOfYear(year);
+        int dayNumber = dayOfYear(month, day, year);
+        return (firstDay + dayNumber - 1) % 7;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        APCalendar calendar = new APCalendar();
+        System.out.println(calendar.dayOfWeek(1, 5, 2019));
+        System.out.println(calendar.dayOfWeek(1, 10, 2019));
+        System.out.println(calendar.firstDayOfYear(2019));
+        System.out.println(calendar.dayOfYear(3,1,2017));
+        System.out.println(calendar.dayOfYear(3,1,2016));
+        System.out.println();
     }
 }
